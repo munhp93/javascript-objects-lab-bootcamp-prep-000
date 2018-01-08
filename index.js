f686491cca0ea61d039c587b1cdec1cf53cf37cf
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-08 04:00:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 var recipes={};
 function updateObjectWithKeyAndValue(object,key,value){
-  return Objects.assign( {}, object, {[key]: value})
+  var new ={};
+  Objects.assign(new, object, {[key]: value})
+  return new
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
-  object[key]=value
-  return object
-}
-function deleteFromObjectByKey(object,key){
-  const newObj = Object.assign({},object)
-  delete newObj[key]
-  return newObj
-}
-function destructivelyDeleteFromObjectByKey(object,key){
-  delete object[key]
-  return object
-}
